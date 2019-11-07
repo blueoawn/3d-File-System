58828d8913fe38713c6a6aeaf1d860d1778e0f85
William Kim <william.kim.176@my.csun.edu>
2019-11-07 14:53:28 -0800

    Added InfoPanel prefab and script

---
+++ 3dFileSystem/Assets/Scripts/InfoPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InfoPanel : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI locationText;
+    public TextMeshProUGUI sizeText;
+    public TextMeshProUGUI typeText;
+
+    //public Image icon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
