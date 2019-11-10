de1a29ada4a2de7fb697424e7f9d6bdd77ad7d4b
William <kim.s.william@gmail.com>
2019-11-09 21:43:40 -0800

    Updated info panel

--- 3dFileSystem/Assets/Scripts/InfoPanel.cs
+++ 3dFileSystem/Assets/Scripts/InfoPanel.cs
@@ -23,4 +23,24 @@ public class InfoPanel : MonoBehaviour
     {
         
     }
+
+    public void fillPanel(DataNode dn)
+    {
+        nameText.SetText(dn.Name);
+        locationText.SetText("Location: " + dn.FullName);
+        sizeText.SetText("Size: " + string.Format("{0:#,##0}", dn.Size) + " bytes");
+        
+        if(dn.IsFolder)
+        {
+            typeText.SetText("Type: Folder");
+        }
+        else if(dn.IsDrive)
+        {
+            typeText.SetText("Type: Drive");
+        }
+        else
+        {
+            typeText.SetText("Type: File");
+        }
+    }
 }
