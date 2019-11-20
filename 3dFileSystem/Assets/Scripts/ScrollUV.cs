6c4c8361f8656c2bf350abc16ea1ceac4940bdb0
Alberto <Alberto.luna.642@my.csun.edu>
2019-11-19 21:39:41 -0800

    Scrolling space background

---
+++ 3dFileSystem/Assets/Scripts/ScrollUV.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollUV : MonoBehaviour {
+
+	void Update () {
+
+		MeshRenderer mr = GetComponent<MeshRenderer>();
+
+		Material mat = mr.material;
+
+		Vector2 offset = mat.mainTextureOffset;
+
+		offset.x += Time.deltaTime / 100f;
+
+		mat.mainTextureOffset = offset;
+
+	}
+
+}
